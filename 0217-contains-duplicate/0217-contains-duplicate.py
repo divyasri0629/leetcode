c3cf18b4017123ebd5c81264f67b446f7c5d38f0
divyasri0629 <153481485+divyasri0629@users.noreply.github.com>
2025-12-01 12:10:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        arr=[]
+        for i in nums:
+            if i in arr:
+                return True
+            arr.append(i)
+        return False
+        
+            
