33e6b4141c3b5ec550e6f200cfc3953794c79332
divyasri0629 <153481485+divyasri0629@users.noreply.github.com>
2025-12-09 22:40:23 +0530

    Time: 18 ms (90.11%), Space: 23.1 MB (41.31%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,22 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+    def hash(self, key):
+        return key % self.size
+
+    def add(self, key: int) -> None:
+        h = self.hash(key)
+        if key not in self.buckets[h]:
+            self.buckets[h].append(key)
+
+    def remove(self, key: int) -> None:
+        h = self.hash(key)
+        if key in self.buckets[h]:
+            self.buckets[h].remove(key)
+
+    def contains(self, key: int) -> bool:
+        h = self.hash(key)
+        return key in self.buckets[h]
