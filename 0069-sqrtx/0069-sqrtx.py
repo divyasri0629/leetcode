77cb0e65e0bd063cc9c59adebcb168ba7abef93f
divyasri0629 <153481485+divyasri0629@users.noreply.github.com>
2025-12-24 11:47:39 +0530

    Time: 3 ms (62.6%), Space: 17.4 MB (94.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,28 +1,17 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # Handle the base case for x = 0
-        if x == 0:
-            return 0
-        
-        left, right = 0, x
-        
-        while left <= right:
-            mid = (left + right) // 2
-            if mid * mid == x:
+        if x==0:
+            return x
+        i=0
+        j=x
+        while i<=j:
+            mid=(i+j)//2
+            if mid*mid==x:
                 return mid
-            elif mid * mid < x:
-                left = mid + 1
+            elif mid*mid<x:
+                i=mid+1
             else:
-                right = mid - 1
+                j=mid-1
+
+        return j
         
-        # After the loop ends, `right` will be the largest integer such that `right * right <= x`
-        return right
-
-# Example usage:
-solution = Solution()
-
-# Test cases
-print(solution.mySqrt(4))  # Output: 2
-print(solution.mySqrt(8))  # Output: 2
-print(solution.mySqrt(16)) # Output: 4
-print(solution.mySqrt(10)) # Output: 3
