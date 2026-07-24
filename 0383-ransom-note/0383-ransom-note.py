07b577b20b66e217080febc275aa14d8f62b00f4
divyasri0629 <153481485+divyasri0629@users.noreply.github.com>
2026-07-24 17:02:25 +0530

    Time: 19 ms (62.27%), Space: 19.6 MB (71.6%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,7 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        a={}
+        for i in magazine:
+            a[i]=a.get(i,0)+1
+            
         for i in ransomNote:
-            if i not in magazine:
+            if i not in a or a[i]==0:
                 return False
-            magazine=magazine.replace(i,"",1)
+            a[i]-=1
         return True
