3c70dd579cdd7c64d033fad76c70c33bb63846b4
divyasri0629 <153481485+divyasri0629@users.noreply.github.com>
2026-07-24 17:02:52 +0530

    Time: 27 ms (33.45%), Space: 19.7 MB (32.93%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -5,7 +5,7 @@ class Solution:
             a[i]=a.get(i,0)+1
             
         for i in ransomNote:
-            if i not in a or a[i]==0:
+            if i not in a or a[i]<=0:
                 return False
             a[i]-=1
         return True
